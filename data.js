68091b790ff61c6a3b8eeb7ad8522f9ee7d2e6d4
diksha-khatri <dikshakhatri2626@gmail.com>
2022-03-08 12:25:18 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,42 @@
+const characterObject = {
+    hero:{
+    name: "Wizard",
+    avtar: "https://media.istockphoto.com/photos/wizard-and-dragon-scene-3d-illustration-picture-id1126269411?k=20&m=1126269411&s=612x612&w=0&h=Z8B9q3XrbPbDImjM8dCJKsMx4l61i7L1I8eOZ8hoqcA=",
+    health: 60,
+    diceCount:3,
+    currentDiceScore: []
+
+   } ,
+   
+   
+    orc:{
+    name: "Orc",
+    avtar: "https://wc.wallpaperuse.com/wallp/73-730197_s.jpg",
+    health: 30,
+    diceCount: 1,
+    currentDiceScore: []
+
+   },
+
+    demon:{
+    name: "demon",
+    avtar: "https://static8.depositphotos.com/1480128/976/i/600/depositphotos_9768956-stock-photo-devil-art-project.jpg",
+    health: 20,
+    diceCount: 2,
+    currentDiceScore: []
+
+   },
+
+    goblin:{
+    name: "goblin",
+    avtar: "https://www.pinkvilla.com/imageresize/green_goblin.jpg?width=752&format=webp&t=pvorg",
+    health: 25,
+    diceCount: 3,
+    currentDiceScore: []
+
+   }
+
+   
+   
+}
+export default characterObject
---
+++ index.js
@@ -0,0 +1,78 @@
+import characterData from './data.js'
+import Character from './character.js'
+
+let monstersArray = ["orc", "demon", "goblin"];
+
+function getNewMonster(){
+   
+   const nextMonsterData = characterData[monstersArray.shift()]
+    return nextMonsterData > "" ? new Character(nextMonsterData) : {}
+
+}
+
+let isWaiting = false 
+function attack(){
+    if(!isWaiting){
+        wizard.getDiceHtml()
+        monster.getDiceHtml()
+        wizard.takeDamage(monster.currentDiceScore)
+        monster.takeDamage(wizard.currentDiceScore)
+        render()
+        if(wizard.dead){
+            endGame()
+        }
+        else if(monster.dead){
+            isWaiting = true
+            if(monstersArray.length > 0){
+                
+                setTimeout(()=>{
+                    monster = getNewMonster()
+                    render()
+                    isWaiting = false
+                },1000)
+                
+            }
+            else{
+                endGame()
+            }
+        }
+    }
+}
+
+function endGame(){
+    isWaiting = true
+    const endMessage = wizard.health === 0 && monster.health === 0 ?
+        "No victors - all creatures are dead" :
+        wizard.health > 0 ? "The Wizard Wins" :
+            "The Monsters are Victorious"
+
+    const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
+    setTimeout(() => document.body.innerHTML = 
+    `<div class="end-game">
+        <h2>Game Over</h2>
+        <h3>${endMessage}</h3>
+        <p class="end-emoji">${endEmoji}</p>
+    </div>`,1500)
+     
+}
+
+
+
+function render() {
+    console.log(wizard)
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
+    console.log(monster)
+    document.getElementById('monster').innerHTML = monster.getCharacterHtml();
+}
+
+document.getElementById("attack-button").addEventListener('click', attack)
+
+
+const wizard = new Character(characterData.hero)
+let monster = getNewMonster()
+render()
+
+
+
+
+
